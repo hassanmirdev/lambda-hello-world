c0f6ccc9bfab0675de78cd6b60f2911747319442
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 23:32:14 +0530

    Update outputs.tf

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -17,7 +17,8 @@ output "log_group_arn" {
   description = "The ARN of the CloudWatch Log Group for the Lambda function."
   value       = aws_cloudwatch_log_group.hello_world2.arn
 }
-# output "lambda_image_uri" {
- # description = "The URI of the Lambda function image stored in Amazon ECR."
-#  value       = aws_lambda_function.hello_world2.image_uri
-# }
+
+output "lambda_image_uri" {
+description = "The URI of the Lambda function image stored in Amazon ECR."
+value       = aws_lambda_function.hello_world2.image_uri
+}
