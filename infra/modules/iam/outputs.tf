e79d46987b900ff58dcdc5c4a176673041a93722
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:24:13 +0530

    Update outputs.tf

--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -1,12 +1,8 @@
-# Outputs file to expose resource information
-
-# Output the IAM role ARN
 output "lambda_role_arn" {
   description = "The ARN of the Lambda execution IAM role."
   value       = aws_iam_role.lambda_exec.arn
 }
 
-# Output the IAM role name
 output "lambda_role_name" {
   description = "The name of the Lambda execution IAM role."
   value       = aws_iam_role.lambda_exec.name
