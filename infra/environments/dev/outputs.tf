b58d446323587baf23ccc3a9e9cd54fd01d705b3
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:14:46 +0530

    Update outputs.tf

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -1,23 +1,19 @@
 output "function_name" {
   description = "Name of the Lambda function."
-
-  value = aws_lambda_function.hello_world2.function_name
+  value       = module.lambda_function.lambda_function_name
 }
 
 output "lambda_function_arn" {
   description = "ARN of the Lambda function."
-
-  value = aws_lambda_function.hello_world2.arn
+  value       = module.lambda_function.lambda_function_arn
 }
 
 output "lambda_function_image_uri" {
   description = "The ECR URI for the Lambda function image."
-
-  value = aws_lambda_function.hello_world2.image_uri
+  value       = module.lambda_function.lambda_image_uri
 }
+
 output "base_url" {
   description = "Base URL for API Gateway stage."
-
-  value = aws_apigatewayv2_stage.lambda.invoke_url
+  value       = module.api_gateway.api_gateway_url
 }
-
