2826d2420ebdcb80a962a888ebeea0fcacfdabfb
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:48:41 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,8 +1,3 @@
 resource "aws_ecr_repository" "hello_world_repo" {
   name = var.ecr_repository_name
 }
-
-output "ecr_repository_uri" {
-  description = "The URI of the ECR repository."
-  value       = aws_ecr_repository.hello_world_repo.repository_url
-}
