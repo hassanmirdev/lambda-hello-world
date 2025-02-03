eabed3c4f1b9b87ea9cadfab18a6a76713034475
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:49:05 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -18,13 +18,3 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = var.lambda_policy_arn
 }
-
-output "lambda_role_arn" {
-  description = "The ARN of the Lambda execution IAM role."
-  value       = aws_iam_role.lambda_exec.arn
-}
-
-output "lambda_role_name" {
-  description = "The name of the Lambda execution IAM role."
-  value       = aws_iam_role.lambda_exec.name
-}
