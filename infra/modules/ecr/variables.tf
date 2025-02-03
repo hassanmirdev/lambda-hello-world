c0de17b78ebba169a4820f6ba1b1ea458ebd7fcf
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:24:48 +0530

    Update variables.tf

--- infra/modules/ecr/variables.tf
+++ infra/modules/ecr/variables.tf
@@ -1,6 +1,3 @@
-# Variables file for defining configurable parameters
-
-# ECR repository name
 variable "ecr_repository_name" {
   description = "The name of the ECR repository."
   type        = string
