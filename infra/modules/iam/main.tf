3fbe865e1f1def976ab53e5373900b614411e525
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 21:40:29 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,21 +1,22 @@
+# IAM Role for Lambda execution
 resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda"
+  name = var.lambda_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Sid       = ""
       Principal = {
         Service = "lambda.amazonaws.com"
       }
-      }
-    ]
+    }]
   })
 }
 
+# Attach the AWSLambdaBasicExecutionRole policy to the Lambda IAM role
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = var.lambda_policy_arn
 }
