38aa75f69d5df1ae09d178926b0fd56bc13fd820
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:15:04 +0530

    Update providers.tf

--- infra/environments/dev/providers.tf
+++ infra/environments/dev/providers.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-  region  = "us-east-1"        # Specify your AWS region
-# access_key = "<AWS_ACCESS_KEY>"  # Replace with your AWS Access Key ID
-# secret_key = "<AWS_SECRET_KEY>"  # Replace with your AWS Secret Access Key
-  # Optionally you can use profile if you have AWS CLI configured with a profile:
-  # profile = "your-profile-name"
+  region  = "us-east-1" 
 }
 
