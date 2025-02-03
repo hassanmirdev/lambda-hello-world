9a778e1eb5c777bb3c68dc5c66fb37d5beca78aa
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:25:02 +0530

    Update outputs.tf

--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -1,6 +1,3 @@
-# Outputs file to expose resource information
-
-# Output the ECR repository URI
 output "ecr_repository_uri" {
   description = "The URI of the ECR repository."
   value       = aws_ecr_repository.hello_world_repo.repository_url
