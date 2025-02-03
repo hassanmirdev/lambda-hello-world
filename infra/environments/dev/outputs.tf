125966ffdb0a87f351cffc3550cdd7c56b2ddd44
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 23:30:45 +0530

    Update outputs.tf

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -17,7 +17,7 @@ output "base_url" {
   description = "Base URL for API Gateway stage."
   value       = module.api_gateway.api_gateway_url
 }
-output "lambda_function_image_uri" {
-  description = "The ECR URI for the Lambda function image."
-  value       = module.lambda_function.lambda_image_uri  # This should work after the output is defined in the module
-}
+# output "lambda_function_image_uri" {
+ # description = "The ECR URI for the Lambda function image."
+ # value       = module.lambda_function.lambda_image_uri  # This should work after the output is defined in the module
+# }
