5c3022a04db2348389cc1f074ac4e1b9fd9a6e07
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 21:37:49 +0530

    Update main.tf

--- infra/modules/api/main.tf
+++ infra/modules/api/main.tf
@@ -1,12 +1,18 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+# API Gateway creation
 resource "aws_apigatewayv2_api" "lambda" {
-  name          = "serverless_lambda_gw"
+  name          = var.api_name
   protocol_type = "HTTP"
 }
 
+# API Gateway Stage with CloudWatch logging configuration
 resource "aws_apigatewayv2_stage" "lambda" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  name        = "serverless_lambda_stage"
+  name        = var.stage_name
   auto_deploy = true
 
   access_log_settings {
@@ -23,37 +29,36 @@ resource "aws_apigatewayv2_stage" "lambda" {
       status                  = "$context.status"
       responseLength          = "$context.responseLength"
       integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
+    })
   }
 }
 
+# Integration of Lambda function with API Gateway
 resource "aws_apigatewayv2_integration" "hello_world" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  integration_uri    = aws_lambda_function.hello_world2.invoke_arn  # Updated to use hello_world2
+  api_id             = aws_apigatewayv2_api.lambda.id
+  integration_uri    = var.lambda_invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
 
+# Route to trigger Lambda
 resource "aws_apigatewayv2_route" "hello_world" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+  api_id   = aws_apigatewayv2_api.lambda.id
+  route_key = var.route_key
+  target   = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
 }
 
+# CloudWatch Log Group for API Gateway
 resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/hello-world2"
-
-  retention_in_days = 30
+  name              = var.log_group_name
+  retention_in_days = var.retention_days
 }
 
+# Lambda permission to allow API Gateway to invoke Lambda
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world2.function_name  # Updated to use hello_world2
+  function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
