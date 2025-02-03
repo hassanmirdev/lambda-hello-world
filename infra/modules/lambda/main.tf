9e1bb7c337de4a109498354657142684011810b7
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:25:25 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -1,17 +1,33 @@
-# Lambda function definition
 resource "aws_lambda_function" "hello_world2" {
   function_name = var.lambda_function_name
 
   package_type = "Image"
   image_uri    = var.lambda_image_uri
 
-  role = aws_iam_role.lambda_exec.arn
+  role = var.lambda_role_arn
 }
 
-# CloudWatch Log Group for the Lambda function
 resource "aws_cloudwatch_log_group" "hello_world2" {
   name              = "/aws/lambda/${aws_lambda_function.hello_world2.function_name}-v2"
   retention_in_days = var.log_group_retention_days
 }
 
+output "lambda_function_name" {
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.hello_world2.function_name
+}
 
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function."
+  value       = aws_lambda_function.hello_world2.arn
+}
+
+output "log_group_name" {
+  description = "The name of the CloudWatch Log Group for the Lambda function."
+  value       = aws_cloudwatch_log_group.hello_world2.name
+}
+
+output "log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group for the Lambda function."
+  value       = aws_cloudwatch_log_group.hello_world2.arn
+}
