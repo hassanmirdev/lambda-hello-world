14156a66375c6bd32572aec22e8109656efdcaf1
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:26:45 +0530

    Update variables.tf

--- infra/modules/api/variables.tf
+++ infra/modules/api/variables.tf
@@ -1,21 +1,11 @@
-# Define variables for reusable parameters
-
 variable "api_name" {
   description = "The name of the API Gateway."
   type        = string
-  default     = "serverless_lambda_gw"
 }
 
-variable "stage_name" {
-  description = "The name of the stage."
+variable "route_key" {
+  description = "The route key for the API Gateway route."
   type        = string
-  default     = "serverless_lambda_stage"
-}
-
-variable "log_group_name" {
-  description = "The name of the CloudWatch Log Group."
-  type        = string
-  default     = "/aws/api_gw/hello-world2"
 }
 
 variable "lambda_function_name" {
@@ -28,14 +18,19 @@ variable "lambda_invoke_arn" {
   type        = string
 }
 
+variable "log_group_name" {
+  description = "The name of the CloudWatch Log Group."
+  type        = string
+}
+
 variable "retention_days" {
-  description = "The retention days for the CloudWatch Log Group."
+  description = "Retention days for CloudWatch Log Group."
   type        = number
   default     = 30
 }
 
-variable "route_key" {
-  description = "The route key for the API Gateway route."
+variable "stage_name" {
+  description = "The name of the API Gateway stage."
   type        = string
-  default     = "GET /hello"
+  default     = "prod"
 }
