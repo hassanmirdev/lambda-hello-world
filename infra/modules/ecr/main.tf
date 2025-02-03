fa7f5c29b6529cf39b13214b1dce2877ea9a2cdb
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:24:35 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,4 +1,8 @@
-# ECR Repository for the Lambda function image
 resource "aws_ecr_repository" "hello_world_repo" {
   name = var.ecr_repository_name
+}
+
+output "ecr_repository_uri" {
+  description = "The URI of the ECR repository."
+  value       = aws_ecr_repository.hello_world_repo.repository_url
 }
