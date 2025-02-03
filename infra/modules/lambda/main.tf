b675fcc7b320febfb19e6e95479ced7b705fea05
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-03 22:49:36 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -11,23 +11,3 @@ resource "aws_cloudwatch_log_group" "hello_world2" {
   name              = "/aws/lambda/${aws_lambda_function.hello_world2.function_name}-v2"
   retention_in_days = var.log_group_retention_days
 }
-
-output "lambda_function_name" {
-  description = "The name of the Lambda function."
-  value       = aws_lambda_function.hello_world2.function_name
-}
-
-output "lambda_function_arn" {
-  description = "The ARN of the Lambda function."
-  value       = aws_lambda_function.hello_world2.arn
-}
-
-output "log_group_name" {
-  description = "The name of the CloudWatch Log Group for the Lambda function."
-  value       = aws_cloudwatch_log_group.hello_world2.name
-}
-
-output "log_group_arn" {
-  description = "The ARN of the CloudWatch Log Group for the Lambda function."
-  value       = aws_cloudwatch_log_group.hello_world2.arn
-}
