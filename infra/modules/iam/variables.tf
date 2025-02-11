4a20ccb38c810e3e0fe9780b57c386b0c73d7872
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-11 13:03:01 +0530

    Update variables.tf

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -10,21 +10,6 @@ variable "lambda_policy_arn" {
   default     = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-variable "retention_days" {
-  description = "Retention period for CloudWatch logs"
-  type        = number
-}
-
-variable "api_name" {
-  description = "The name of the API Gateway"
-  type        = string
-}
-
-variable "log_group_name" {
-  description = "The name of the CloudWatch log group"
-  type        = string
-}
-
 variable "lambda_role_name" {
   description = "Name of the Lambda execution role"
   type        = string
