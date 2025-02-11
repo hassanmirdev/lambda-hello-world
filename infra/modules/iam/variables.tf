382b7700edebce820f443081e4c93463f61144da
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-11 12:58:51 +0530

    Update variables.tf

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -1,7 +1,7 @@
 variable "lambda_role_name" {
   description = "The name of the IAM role for Lambda execution."
   type        = string
-  default     = "serverless_lambda"
+
 }
 
 variable "lambda_policy_arn" {
@@ -11,19 +11,21 @@ variable "lambda_policy_arn" {
 }
 
 variable "retention_days" {
-  description = "Retention period in days for CloudWatch logs"
+  description = "Retention period for CloudWatch logs"
   type        = number
-  default     = 30  # Adjust this value based on your preference
 }
 
 variable "api_name" {
   description = "The name of the API Gateway"
   type        = string
-  default     = "my-api"  # Set a default name or pass it dynamically
 }
 
 variable "log_group_name" {
-  description = "The name of the CloudWatch Log Group"
+  description = "The name of the CloudWatch log group"
   type        = string
-  default     = "api-gateway-log-group"  # Set a default log group name or pass dynamically
+}
+
+variable "lambda_role_name" {
+  description = "Name of the Lambda execution role"
+  type        = string
 }
