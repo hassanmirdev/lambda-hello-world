fa2f9c485cb2375ee47bd48b4f1cdb2fafe09d89
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-15 15:19:25 +0530

    Update main.tf

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -21,7 +21,7 @@ module "lambda_function" {
   source = "../../modules/lambda"  # Path to the Lambda module
 
   lambda_function_name = "hello-world2"  # Lambda function name
-  lambda_image_uri     = "677276078111.dkr.ecr.us-east-1.amazonaws.com/hello-world:latest"  # ECR image URI for Lambda
+  lambda_image_uri     = "677276078111.dkr.ecr.us-east-1.amazonaws.com/lambda-hello-world:latest"  # ECR image URI for Lambda
   lambda_role_arn      = module.iam_lambda.lambda_role_arn  # IAM role ARN for Lambda
 }
 
