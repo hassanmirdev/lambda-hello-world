b51cb40c0d55450a5004696af9aaef420b15e5d3
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-15 16:07:20 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "hello_world2" {
   role = var.lambda_role_arn
 }
 
-resource "aws_cloudwatch_log_group" "hello_world2" {
+resource "aws_cloudwatch_log_group" "hello_world3" {
   name              = "/aws/lambda/${aws_lambda_function.hello_world2.function_name}-v2"
   retention_in_days = var.log_group_retention_days
 }
