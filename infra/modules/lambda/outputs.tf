b807bad0b53a2efb5c05dea12e4d94527013da2b
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-15 16:19:18 +0530

    Update outputs.tf

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -1,24 +1,24 @@
 output "lambda_function_name" {
   description = "The name of the Lambda function."
-  value       = aws_lambda_function.hello_world2.function_name
+  value       = aws_lambda_function.hello_world3.function_name
 }
 
 output "lambda_function_arn" {
   description = "The ARN of the Lambda function."
-  value       = aws_lambda_function.hello_world2.arn
+  value       = aws_lambda_function.hello_world3.arn
 }
 
 output "log_group_name" {
   description = "The name of the CloudWatch Log Group for the Lambda function."
-  value       = aws_cloudwatch_log_group.hello_world2.name
+  value       = aws_cloudwatch_log_group.hello_world3.name
 }
 
 output "log_group_arn" {
   description = "The ARN of the CloudWatch Log Group for the Lambda function."
-  value       = aws_cloudwatch_log_group.hello_world2.arn
+  value       = aws_cloudwatch_log_group.hello_world3.arn
 }
 
 output "lambda_image_uri" {
 description = "The URI of the Lambda function image stored in Amazon ECR."
-value       = aws_lambda_function.hello_world2.image_uri
+value       = aws_lambda_function.hello_world3.image_uri
 }
