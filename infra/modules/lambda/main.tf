4f77bba9448a87c82aaaa6ca72decc3f89f0548b
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-24 18:00:42 +0530

    Update main.tf

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -1,3 +1,4 @@
+# Creates a Lambda function with the specified Docker image URI and role for execution
 resource "aws_lambda_function" "hello_world3" {
   function_name = var.lambda_function_name
 
@@ -7,6 +8,7 @@ resource "aws_lambda_function" "hello_world3" {
   role = var.lambda_role_arn
 }
 
+# Creates a CloudWatch Log Group to store logs for the Lambda function with defined retention period
 resource "aws_cloudwatch_log_group" "hello_world3" {
   name              = "/aws/lambda/${aws_lambda_function.hello_world3.function_name}-v2"
   retention_in_days = var.log_group_retention_days
