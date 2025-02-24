fd056ef714b8a8c1454996d94dcbd4e999da1684
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-24 17:57:01 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,3 +1,4 @@
+# Creates an IAM role allowing AWS Lambda service to assume the role
 resource "aws_iam_role" "lambda_exec" {
   name = var.lambda_role_name
 
@@ -16,7 +17,7 @@ resource "aws_iam_role" "lambda_exec" {
 }
 
 
-# Attach AWSLambdaBasicExecutionRole policy to allow logging to CloudWatch
+# Attaches the AWSLambdaBasicExecutionRole policy to the IAM role to grant permissions for logging to CloudWatch
 resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_exec.name
